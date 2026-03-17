79613859be5d6c71acb7b5f5601c37124e13605b
OUSMANE Mohamed Sadjad <ousmanesadjad@gmail.com>
2026-03-17 11:38:26 +0100

    fix(ci): disable body-max-line-length rule to allow semantic-release commit bodies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // semantic-release generates commit bodies with changelog lines that exceed 100 chars
+    'body-max-line-length': [0],
+  },
 };
